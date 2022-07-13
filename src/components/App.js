859535564fd5b0db739c69074a2cca0ca7ab5fdc
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-13 10:38:14 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+// import * as RMFunctions from "../data/data"
+
+// console.log( RMFunctions);
 
 function App() {
   return (
